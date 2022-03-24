19915d6d588edd86df6b0e2071237119c2276b3b
Walter <zikavaclav05@gmail.com>
2022-03-24 19:59:55 +0100

    js_fix

--- static/js/main.js
+++ static/js/main.js
@@ -105,29 +105,3 @@ function SetDropdownAresTest(output_id, input_data) {
   document.getElementById(output_id).innerText = "Dovyplnit \"" + input_data.value + "\"";
 
 }
-
-var http = require('http');
-
-function startKeepAlive() {
-    setInterval(function() {
-        var options = {
-            host: 'fakturujto.herokuapp.com',
-            port: 80,
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
-    }, 20 * 60 * 1000); // load every 20 minutes
-}
-
-startKeepAlive();
